01f8352336a6169640fe7f9f82bf92508b8a2e4d
juma emmanuel <jumaemmanuel23@0utlook.com>
2023-08-10 04:25:38 -0700

    hot fix for betty style

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int my_int;
 long int my_longint;
 long long int my_longlongint;
 float my_float;
-printf("Size of a char: %zu byte(s)\n",sizeof(my_char));
-printf("Size of an int: %zu byte(s)\n",sizeof(my_int));
-printf("Size of a long int: %zu byte(s)\n",sizeof(my_longint));
-printf("Size of a long long int: %zu byte(s)\n",sizeof(my_longlongint));
-printf("Size of a float: %zu byte(s)\n",sizeof(my_float));
+printf("Size of a char: %zu byte(s)\n", sizeof(my_char));
+printf("Size of an int: %zu byte(s)\n", sizeof(my_int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(my_longint));
+printf("Size of a long long int: %zu byte(s)\n" ,sizeof(my_longlongint));
+printf("Size of a float: %zu byte(s)\n", sizeof(my_float));
 return (0);
 }
