3d70a1f9e0eff302ee69408a86201cbdde005fb4
Juma Emmanuel <jumaemmanuel462@yahoo.com>
2023-08-11 09:56:12 -0700

    hot fix

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,10 +9,10 @@ int main(void)
 {
 char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
-	 putchar(ch);
+	putchar(ch);
 for (ch = 'A'; ch <= 'Z'; ch++)
-	 putchar(ch);
+	putchar(ch);
 putchar('\n');
-return 0;
+return (0);
 }
 
