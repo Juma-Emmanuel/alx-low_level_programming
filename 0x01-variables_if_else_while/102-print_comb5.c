4c0ede4a4f1cea6ff56c65826c75098a1504b988
Juma Emmanuel <jumaemmanuel462@yahoo.com>
2023-08-11 12:33:14 -0700

    hot fix

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,18 +11,18 @@ int j, k;
 for (j = 0; j <= 98; j++)
 {
 	for (k = j + 1; k <= 99; k++)
-		{
-			putchar((j / 10) + '0');
-			putchar((j % 10) + '0');
-			putchar(' ');
-			putchar((k / 10) + '0');
-			putchar((k % 10) + '0');
-			if (j == 98 && k == 99)
-				continue;
-			putchar(',');
-			putchar(' ');
-		}
+	{
+		putchar((j / 10) + '0');
+		putchar((j % 10) + '0');
+		putchar(' ');
+		putchar((k / 10) + '0');
+		putchar((k % 10) + '0');
+		if (j == 98 && k == 99)
+			continue;
+		putchar(',');
+		putchar(' ');
 	}
+}
 putchar('\n');
 return (0);
 }
