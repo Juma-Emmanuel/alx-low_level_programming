72f7608c021c009e2783750f6ca0a8161ef18c6a
Juma Emmanuel <jumaemmanuel462@yahoo.com>
2023-08-11 12:05:01 -0700

    hot fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main  
- *prints all possible different combinations of two digits
+ * main - prints all possible different combinations of two digits 
+ *
  * Return: Always 0.
  */
 int main(void)
