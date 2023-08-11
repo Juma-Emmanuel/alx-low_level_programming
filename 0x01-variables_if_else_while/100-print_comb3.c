e7dbe642d0e69b3ce82a25d9ac9cfdcb8855c374
Juma Emmanuel <jumaemmanuel462@yahoo.com>
2023-08-11 12:03:04 -0700

    hot fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,23 +7,19 @@
  */
 int main(void)
 {
-	int num1, num2;
-
-	for (num1 = 0; num1 < 9; num1++)
+int num1, num2;
+for (num1 = 0; num1 < 9; num1++)
+{
+	for (num2 = num1 + 1; num2 < 10; num2++)
 	{
-		for (num2 = num1 + 1; num2 < 10; num2++)
-		{
-			putchar((num1 % 10) + '0');
-			putchar((num2 % 10) + '0');
-
-			if (num1 == 8 && num2 == 9)
-				continue;
-			putchar(',');
-			putchar(' ');
-		}
+		putchar((num1 % 10) + '0');
+		putchar((num2 % 10) + '0');
+		if (num1 == 8 && num2 == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
 	}
-
-	putchar('\n');
-
-	return (0);
+}
+putchar('\n');
+return (0);
 }
