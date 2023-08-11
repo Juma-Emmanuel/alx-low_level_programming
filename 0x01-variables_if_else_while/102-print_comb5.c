33b8b105a6a36da6d585e62a9d3d8cc1c815e026
Juma Emmanuel <jumaemmanuel462@yahoo.com>
2023-08-11 12:27:34 -0700

    hot fix

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,32 +1,29 @@
+#include <stdio.h>
 /**
- * prints all possible combinations of two two-digit numbers.
+ * main - prints all possible combinations of two two-digit numbers.
+ *
  * 
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int j, k;
-
-	for (j = 0; j <= 98; j++)
-	{
-		for (k = j + 1; k <= 99; k++)
+int j, k;
+for (j = 0; j <= 98; j++)
+{
+	for (k = j + 1; k <= 99; k++)
 		{
 			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
 			putchar(' ');
 			putchar((k / 10) + '0');
 			putchar((k % 10) + '0');
-
 			if (j == 98 && k == 99)
 				continue;
-
 			putchar(',');
 			putchar(' ');
 		}
 	}
-
-	putchar('\n');
-
-	return (0);
+putchar('\n');
+return (0);
 }
