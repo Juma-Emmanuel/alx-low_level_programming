52026e91d9603ee9751721d549bfe954af9544a2
Juma Emmanuel <jumaemmanuel462@yahoo.com>
2023-08-11 09:20:39 -0700

    hot fix

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -
+ *Prints the alphabet in lowercase.
+ * Return: Always 0.
+ */
+int main(void)
+{
+for (char ch = 'a'; ch <= 'z'; ch)
+	putchar(ch);
+putchar('\n');
+return (0);
+}
