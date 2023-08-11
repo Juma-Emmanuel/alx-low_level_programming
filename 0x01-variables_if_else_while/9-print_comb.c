086b75d184e281680e84d7459e0f099fae6e6dbd
juma emmanuel <jumaemmanuel23@0utlook.com>
2023-08-11 04:31:28 -0700

    created new file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0 ...
+ */
+int main(void)
+{
+	int q;
+
+	for (q = 0; q <= 9; q++)
+	{
+		putchar((q % 10) + '0');
+		if (q == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
