7c0e33e4e89228696a703517147d73e92640a259
Juma Emmanuel <jumaemmanuel462@yahoo.com>
2023-08-15 12:37:17 -0700

    created new file

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+* times_table - that prints the 9 times table
+*
+* Return: no return.
+*/
+void times_table(void)
+{
+	int x, y, z;
+
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar(48);
+		for (y = 1; y <= 9; y++)
+		{
+			z = x * y;
+			_putchar(44);
+			_putchar(32);
+			if (z <= 9)
+			{
+				_putchar(32);
+				_putchar(z + 48);
+			}
+			else
+			{
+				_putchar((z / 10) + 48);
+				_putchar((z % 10) + 48);
+			}
+		}
+	_putchar('\n');
+	}
+}
