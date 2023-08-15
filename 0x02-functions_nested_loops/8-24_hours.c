1647a2dd651405e407611c314a6660451f1dee6d
Juma Emmanuel <jumaemmanuel462@yahoo.com>
2023-08-15 12:25:41 -0700

    created new fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,5 +1,3 @@
-#include "main.h"
-
 /**
 * jack_bauer - prints every minute of the day
 *
@@ -10,24 +8,24 @@ void jack_bauer(void)
 int j, k, l, m;
 for (j = 48; j <= 50; j++)
 {
-	for (k = 48; k<= 57; k++)
-	{
-		for (l = 48; l <= 53; l++)
-		{
-			for (m = 48; m <= 57; m++)
-			{
-				if (j >= 50 && k>= 52)
-				{
-					break;
-			                _putchar(j);
-					_putchar(k);
-					_putchar(58);
-					_putchar(l);
-					_putchar(m);
-					_putchar('\n');
-				}
-			}
-		}
-	}
+    for (k = 48; k<= 57; k++)
+    {
+        for (l = 48; l <= 53; l++)
+        {
+            for (m = 48; m <= 57; m++)
+            {
+                if (j >= 50 && k>= 52)
+                {
+                    break;
+                    _putchar(j);
+                    _putchar(k);
+                    _putchar(58);
+                    _putchar(l);
+                    _putchar(m);
+                    _putchar('\n');
+                }
+            }
+        }
+    }
 }
 }
