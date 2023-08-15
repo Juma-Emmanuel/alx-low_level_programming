62810922d37c44d89f9f26e2fefa264dd77bf17b
Juma Emmanuel <jumaemmanuel462@yahoo.com>
2023-08-15 07:29:24 -0700

    hot fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,7 +9,11 @@ void print_alphabet(void)
 {
 char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
-	_putchar(ch);
-
-_putchar('\n');
+	putchar(ch);
+putchar('\n');
+}
+int main(void)
+{
+print_alphabet();
+return (0);
 }
