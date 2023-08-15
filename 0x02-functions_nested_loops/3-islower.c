a9eaafa4a0ac7e57a95785ea8784a04b5e872c9b
Juma Emmanuel <jumaemmanuel462@yahoo.com>
2023-08-15 10:25:02 -0700

    hot fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,14 +2,18 @@
 #include "main.h"
 /**
 **_islower - Entry point
-*Prints the alphabet in lowercase.
+* @c: The character in ASCII code
 * Return: Always 0.
 */
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-		return (1);
-        else
-		return (0);
+if (c >= 'a' && c <= 'z')
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
 }
 
