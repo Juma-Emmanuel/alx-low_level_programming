b256f535b8363585f6e418e90af942a58e5c279a
Juma Emmanuel <jumaemmanuel462@yahoo.com>
2023-08-15 07:32:03 -0700

    hot fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,15 +5,15 @@
 *Prints the alphabet in lowercase.
 * Return: Always 0.
 */
+int main(void)
+{
+print_alphabet();
+return (0);
+}
 void print_alphabet(void)
 {
 char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 	putchar(ch);
 putchar('\n');
-}
-int main(void)
-{
-print_alphabet();
-return (0);
 }
