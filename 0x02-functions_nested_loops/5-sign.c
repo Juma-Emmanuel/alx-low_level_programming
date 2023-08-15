0730f626877684968e29306eb176b50636708f9d
Juma Emmanuel <jumaemmanuel462@yahoo.com>
2023-08-15 10:46:02 -0700

    created new file

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+** print_sign - Entry point
+* @n: The sign of the charcer
+* Return: Always 0.
+*/
+int _islower(int c)
+{
+if (n > 0)
+{
+	return (1);
+	_putchar(43);
+}
+else if(n == 0)
+{
+	return (0);
+	_putchar(48);
+}
+else
+{
+	return (-1);
+	_putchar(45);
+}
+}
