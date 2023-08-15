f8630dab3ddee5039ea1737dd7a0ab7a11ad61a5
Juma Emmanuel <jumaemmanuel462@yahoo.com>
2023-08-15 10:03:03 -0700

    hot fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include "main.h"
 /**
-**print_alphabet - Entry point
-*Prints 10mtimes the alphabet in lowercase.
+**print_alphabet_x10 - Entry point
+*Prints 10 times the alphabet in lowercase.
 * Return: Always 0.
 */
 void print_alphabet_x10(void)
 {
 int k;
 int j;
-for (j = 0; j <= 10; j++)
+for (j = 0; j < 10; j++)
 {
 	for (k = 97; k < 123; k++)
 		_putchar(k);
