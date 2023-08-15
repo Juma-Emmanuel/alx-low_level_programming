f15e041b9068ef224823c0a1c075e4f493b4dafb
Juma Emmanuel <jumaemmanuel462@yahoo.com>
2023-08-15 06:30:38 -0700

    hot fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,9 +4,10 @@
  *
  * Description: this program prints _putchar
  *
- * Return:  0 means  (Success) 
- */
-int main(void){
+ *  Return:  0 means  (Success)
+ **/
+int main(void)
+{
 char text[] = "_putchar\n";
 int length = sizeof(text) - 1;
 write(1, text, length);
