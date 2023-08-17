9a18c6bc3791882121296a9fada96ef18b5f6d2d
Juma Emmanuel <jumaemmanuel462@yahoo.com>
2023-08-17 06:21:55 -0700

    hot fix

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
 #include "main.h"
-/*
-*print_line - Write a function that draws a straight line in the terminal.
-*@n: number of characters
-* Return: Always 0.
+/**
+* print_line - Write a function that draws a straight line in the terminal.
+* @n: Integer amount of _
 */
 void print_line(int n)
 {
-int k;
-for (k = 1; k <= n; k++)
-	_putchar(95);
-_putchar('\n');
+	int k;
+
+	for (k = 0; k < n; k++)
+		_putchar(95);
+	_putchar('\n');
 }
