dce489ae9f380527a8b6c7561117f824e69e73df
Juma Emmanuel <jumaemmanuel462@yahoo.com>
2023-08-17 02:47:01 -0700

    created new file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**print_line - Entry point
+*draws a straight line in the terminal.
+* Return: Always 0.
+*/
+void print_line(int n)
+{
+int k;
+for (k = 1; k <= n; k++)
+	_putchar(95);
+_putchar('\n');
+}
