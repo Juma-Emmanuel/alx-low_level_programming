199d40009d9442182633764121f6e962ed0c0d36
Juma Emmanuel <jumaemmanuel462@yahoo.com>
2023-08-17 01:50:51 -0700

    created new file

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+**print_most_numbers - Entry point
+*prints the numbers, from 0 to 9 except 2 and 4
+* Return: Always 0.
+*/
+void print_most_numbers(void)
+{
+int k;
+for (k = 48; k < 58; k++)
+        if (k != 50 && k != 52)
+        {
+                _putchar(k);
+        }
+_putchar('\n');
+}
+
