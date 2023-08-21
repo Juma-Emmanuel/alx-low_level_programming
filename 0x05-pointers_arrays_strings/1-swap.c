e559cd33fbf6a333a2ca5a8e3c009334ca9d3470
Juma Emmanuel <jumaemmanuel462@yahoo.com>
2023-08-21 13:17:44 -0700

    created new file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+**swap_int - Entry point
+*@a: int input
+*@b: - int input
+*swaps the values of two integers
+* Return: Always 0.
+*/
+void swap_int(int *a, int *b)
+{
+        int temp = *a;
+        *a = *b;
+        *b = temp;
+}
