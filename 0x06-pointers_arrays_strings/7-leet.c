120ff0600c3b572f2f16b854f7213120addedd94
Juma Emmanuel <jumaemmanuel462@yahoo.com>
2023-08-23 04:56:02 -0700

    Mozart composed his music not for the elite, but for everybody

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+/**
+**leet - Entry point
+*@str:- string input
+*encodes a string into 1337.
+* Return: Always 0.
+*/
+char *leet(char *str)
+{
+	int i;
+	int size = strlen(str);
+
+	for (i = 0; i < size; i++)
+	{
+		if (str[i] == 'a' || str[i] == 'A')
+			str[i] = '4';
+		else if (str[i] == 'e' || str[i] == 'E')
+			str[i] = '3';
+		else if (str[i] == 'o' || str[i] == 'O')
+			str[i] = '0';
+		else if (str[i] == 't' || str[i] == 'T')
+			str[i] = '7';
+		else if (str[i] == 'l' || str[i] == 'L')
+			str[i] = '1';
+	}
+	return (str);
+}
