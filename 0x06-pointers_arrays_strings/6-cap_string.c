3fbb9a8175d146e29710469cfb2af7ba7b3a91f0
Juma Emmanuel <jumaemmanuel462@yahoo.com>
2023-08-23 04:28:15 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+**cap_string - Entry point
+*@str:- string input
+*capitalizes all words of a string.
+* Return: Always 0.
+*/
+char *cap_string(char *str)
+{
+	int i = 0;
+	
+	while (str[i] != '\0')
+	{
+		if (str[i] == '\t' || str[i] == '\n' || str[i] == ' ' || str[i] == '.' || str[i] == ',' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+		{
+			i++;
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] -= 32;
+		}
+		else
+			i++;
+	}
+	return (str);
+}
