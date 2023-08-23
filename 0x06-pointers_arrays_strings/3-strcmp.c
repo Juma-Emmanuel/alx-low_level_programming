fd0cb494ec93ef05b388c8be61340b48e1fa52d5
Juma Emmanuel <jumaemmanuel462@yahoo.com>
2023-08-23 01:39:42 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+**_strcmp - Entry point
+*@s1: - string input
+*@s2: - string input
+*compares two strings
+* Return: Always 0.
+*/
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
