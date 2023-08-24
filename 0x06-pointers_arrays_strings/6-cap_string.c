b34411941174282fec9fbe2aa3ac90d0475f22aa
Juma Emmanuel <jumaemmanuel462@yahoo.com>
2023-08-24 11:47:36 -0700

    fixes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,24 +1,20 @@
 #include "main.h"
+#include <ctype.h>
 /**
-**cap_string - Entry point
+*cap_string - Entry point
 *@str:- string input
 *capitalizes all words of a string.
 * Return: Always 0.
-*/
+**/
 char *cap_string(char *str)
 {
-	int i = 0;
-	
-	while (str[i] != '\0')
+	int i;
+
+	for (i = 0; str[i]; i++)
 	{
-		if (str[i] == '\t' || str[i] == '\n' || str[i] == ' ' || str[i] == '.' || str[i] == ',' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
-		{
-			i++;
-			if (str[i] >= 'a' && str[i] <= 'z')
-				str[i] -= 32;
-		}
-		else
-			i++;
+		if (isalpha(str[i]) && (i == 0 || !isalpha(str[i - 1])))
+			str[i] = toupper(str[i]);
 	}
 	return (str);
 }
+
