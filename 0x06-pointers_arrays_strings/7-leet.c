81021c4d8f3f96ae3e1915f5c3b88cec5e0199e6
Juma Emmanuel <jumaemmanuel462@yahoo.com>
2023-08-24 20:28:21 -0700

    hot fix

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,28 +1,43 @@
 #include "main.h"
 #include <string.h>
+#include <ctype.h>
+
 /**
-**leet - Entry point
-*@str:- string input
-*encodes a string into 1337.
-* Return: Always 0.
-*/
+ * leet - Entry point
+ * @str: String input
+ * Encodes a string into 1337.
+ * Return: Always 0.
+ */
 char *leet(char *str)
 {
 	int i;
 	int size = strlen(str);
+	char lookup[128];
+
+	for (i = 0; i < 128; i++)
+	{
+		lookup[i] = i;
+	}
+	lookup['a'] = '4';
+	lookup['A'] = '4';
+	lookup['e'] = '3';
+	lookup['E'] = '3';
+	lookup['o'] = '0';
+	lookup['O'] = '0';
+	lookup['t'] = '7';
+	lookup['T'] = '7';
+	lookup['l'] = '1';
+	lookup['L'] = '1';
 
 	for (i = 0; i < size; i++)
 	{
-		if (str[i] == 'a' || str[i] == 'A')
-			str[i] = '4';
-		else if (str[i] == 'e' || str[i] == 'E')
-			str[i] = '3';
-		else if (str[i] == 'o' || str[i] == 'O')
-			str[i] = '0';
-		else if (str[i] == 't' || str[i] == 'T')
-			str[i] = '7';
-		else if (str[i] == 'l' || str[i] == 'L')
-			str[i] = '1';
+		unsigned char index = (unsigned char)str[i];
+
+		if (isalpha(str[i]))
+		{
+			str[i] = lookup[index];
+		}
 	}
 	return (str);
 }
+
