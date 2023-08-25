6fcea29e73caa57849b9600b2800cf47f969f45f
Juma Emmanuel <jumaemmanuel462@yahoo.com>
2023-08-24 19:47:14 -0700

    hot fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,29 +1,35 @@
 #include "main.h"
 #include <string.h>
+
 /**
-**cap_string - Entry point
-*@str:- string input
-*capitalizes all words of a string.
-* Return: Always 0.
-*/
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to capitalize.
+ *
+ * Return: The capitalized string.
+ */
 char *cap_string(char *str)
 {
-        int i = 0;
+	int i = 0;
 
-        while (str[i] != '\0')
-        {
-                if (str[i] == '\t' || str[i] == '\n' || str[i] == ' ' || str[i] == '.' || 
-                	str[i] == ',' || str[i] == '!' || str[i] == '?' || str[i] == '"' ||
-                	 str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
-                {
-                        i++;
-                        if (str[i] >= 'a' && str[i] <= 'z')
-                                str[i] -= 32;
-                }
-                else
-                        i++;
-        }
-        return (str);
+	while (str[i] != '\0')
+	{
+		if (str[i] == '\t' || str[i] == '\n' || str[i] == ' ' ||
+		    str[i] == '.' || str[i] == ',' || str[i] == '!' ||
+		    str[i] == '?' || str[i] == '"' || str[i] == '(' ||
+		    str[i] == ')' || str[i] == '{' || str[i] == '}')
+		{
+			i++;
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] -= 32;
+			}
+		}
+		else
+		{
+			i++;
+		}
+	}
+
+	return (str);
 }
-
 
