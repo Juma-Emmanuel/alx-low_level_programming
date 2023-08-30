12c98a3de3ef15a7ee0d122d06eb152149613504
Juma Emmanuel <jumaemmanuel462@yahoo.com>
2023-08-30 14:50:43 -0700

    created new file

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,12 +7,13 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+        if (*s != '\0')
+        {
+                _print_rev_recursion(s + 1);
+                _putchar(*s);
+        }
+	else
 	{
 		_putchar('\n');
-		return;
 	}
-	_print_rev_recursion(s + 1);
-	_putchar(*s);
 }
-
