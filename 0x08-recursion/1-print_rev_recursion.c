96770247657578a70a8037f6c5e42a4acf2ae9ee
Juma Emmanuel <jumaemmanuel462@yahoo.com>
2023-08-30 13:44:46 -0700

    created new file

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_print_rev_recursion - prints a string, to stdout
+ *@s: input string
+ *Return: not.
+ */
+void _print_rev_recursion(char *s)
+{
+        if (*s == '\0')
+        {
+                _putchar('\n');
+                return;
+        }
+        _print_rev_recursion(s + 1);
+        _putchar(*s);        
+}
+
