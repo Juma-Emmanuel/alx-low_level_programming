e8ed5b2a2f8be6dab3b730483327e783f087e4eb
Juma Emmanuel <jumaemmanuel462@yahoo.com>
2023-08-30 18:51:38 -0700

    hot fix

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,11 +7,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-        if (*s == '\0')
-        {
-                _putchar('\n');
-		return;
-        }
-	_print_rev_recursion(s + 1);
-	_putchar(*s);
+        if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
