0c05334386b050620ef18887fd5cfdcc39c9723e
Juma Emmanuel <jumaemmanuel462@yahoo.com>
2023-08-30 19:03:07 -0700

    hot fix

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,13 +7,12 @@
  */
 int _strlen_recursion(char *s)
 {
-        int length = 0;
+	int length = 0;
 
-        if (*s)
-        {
-                length++;
-                length += _strlen_recursion(s + 1);
-        }
-
-        return (length);
+	if (*s)
+	{
+		length++;
+		length += _strlen_recursion(s + 1);
+	}
+	return (length);
 }
