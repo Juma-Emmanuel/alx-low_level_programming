5cf46b8e7b8d341072a93bbcb473ade172102e0e
Juma-Emmanuel <jumaemmanuel462@yahoo.com>
2023-09-14 00:20:50 +0300

    fixes

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,10 +4,12 @@
  *print_name - prints a name
  *@name: starting address of memory to be filled
  *@f: the desired value. arrays is cool
+ *Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
+
 	f(name);
 }
