07615bb2512ce826a192e4d77ab9050348b347e0
Juma-Emmanuel <jumaemmanuel462@yahoo.com>
2023-09-14 00:13:25 +0300

    fixes

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 /**
  *print_name - prints a name
  *@name: starting address of memory to be filled
@@ -6,5 +7,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL|| f== NULL)
+		return;
 	f(name);
 }
