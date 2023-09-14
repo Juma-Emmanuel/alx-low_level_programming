c7e74c04c5258e26d1f39c1ce8597872236d1398
Juma-Emmanuel <jumaemmanuel462@yahoo.com>
2023-09-14 10:17:04 +0300

    What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-
 /**
  *print_name - prints a name
  *@name: starting address of memory to be filled
@@ -10,5 +9,5 @@ void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
-	f(name);
+	(*f)(name);
 }
