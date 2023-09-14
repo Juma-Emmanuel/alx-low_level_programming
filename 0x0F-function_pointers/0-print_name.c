7644a9fa734dab798a7a0b1b12de80519daf8bb8
Juma-Emmanuel <jumaemmanuel462@yahoo.com>
2023-09-14 11:05:06 +0300

    never get it done

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,28 @@
 #include "function_pointers.h"
+
+#include <stdio.h>
+
 /**
- *print_name - prints a name
- *@name: starting address of memory to be filled
- *@f: the desired value. arrays is cool
- *Return: nothing
- */
+
+ * print_name - print name using pointer to function
+
+ * @name: string to add
+
+ * @f: pointer to function
+
+ * Return: nothing
+
+ **/
+
 void print_name(char *name, void (*f)(char *))
+
 {
-	if (name == NULL || f == NULL)
-		return;
-	(*f)(name);
+
+        if (name == NULL || f == NULL)
+
+                return;
+
+
+        f(name);
+
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,13 +1,20 @@
-#ifndef FUNCTION_POINTERS
-#define FUNCTION_POINTERS
+#ifndef FUNCTION_POINTERS_H
+
+#define FUNCTION_POINTERS_H
+
+
 #include <stddef.h>
 
-#include <stdio.h>
 #include <stdlib.h>
 
-int _putchar(char c);
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int  int_index(int *array, int size, int (*cmp)(int));
 
-#endif 
+void print_name(char *name, void (*f)(char *));
+
+int _putchar(char c);
+
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+int int_index(int *array, int size, int (*cmp)(int));
+
+
+#endif
