da31b4612e5c6ad66a35c9be902ae899edadf65b
Juma-Emmanuel <jumaemmanuel462@yahoo.com>
2023-09-14 09:09:22 +0300

    t

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,7 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-#include <stddef.h>
-#include <stdlib.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 #endif /* FUNCTION_POINTERS_H */
