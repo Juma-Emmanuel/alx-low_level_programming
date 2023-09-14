c3b1a11afec23c405e69db76241ca98793ce3c7b
Juma-Emmanuel <jumaemmanuel462@yahoo.com>
2023-09-14 14:21:27 +0300

    I dont wanna be a player no more

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,11 +12,11 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
-	unsigned int q, sum = 0;
+	unsigned int i, sum = 0;
 
 	va_start(ap, n);
 
-	for (q = 0; q < n; q++)
+	for (i = 0; i < n; i++)
 		sum += va_arg(ap, int);
 
 	va_end(ap);
