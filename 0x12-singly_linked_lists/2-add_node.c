2996fc9b7c39350db9ce08e12ebe229e10f7d58d
Juma Emmanuel <jumaemmanuel462@gmail.com>
2023-09-22 05:48:40 +0300

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,40 +1,30 @@
-#include "lists.h"
+#include <stdlib.h>
 #include <string.h>
+#include "lists.h"
 
 /**
- * add_node - Adds a new node at the beginning
- *            of a list_t list.
- * @head: A pointer to the head of the list_t list.
- * @str: The string to be added to the list_t list.
+ * add_node - adds a new node at the beginning of a linked list
+ * @head: double pointer to the list_t list
+ * @str: new string to add in the node
  *
- * Return: If the function fails - NULL.
- *         Otherwise - the address of the new element.
+ * Return: the address of the new element, or NULL if it fails
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	char *dup_str;
-	int len;
+	unsigned int len = 0;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
-
-	dup_str = strdup(str);
-	if (dup_str == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-
-	for (len = 0; str[len];)
+	while (str[len])
 		len++;
 
-	new->str = dup_str;
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+
+	new->str = strdup(str);
 	new->len = len;
-	new->next = *head;
+	new->next = (*head);
+	(*head) = new;
 
-	*head = new;
-
-	return (new);
+	return (*head);
 }
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -48,3 +48,4 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	return (*head);
 }
+
