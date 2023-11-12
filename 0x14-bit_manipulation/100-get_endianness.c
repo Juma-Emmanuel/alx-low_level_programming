827caa784e292e75ee7e077ae6770ddb8c5170bc
Juma-Emmanuel <jumaemmanuel462@yahoo.com>
2023-11-12 07:47:43 +0300

    6. Endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness of the system
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int num = 1;
+	char *byte = (char *)&num;
+
+	if (*byte)
+		return (1);
+	else
+		return (0);
+}
