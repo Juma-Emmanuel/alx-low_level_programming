8e6f00dbc7f5605777d33a9c38f2d3edef62747b
Juma-Emmanuel <jumaemmanuel462@yahoo.com>
2023-11-16 12:14:34 +0300

    4. elf

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,7 +5,18 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char class);
+void print_data(unsigned char data);
+void print_version(unsigned char version);
+void print_osabi(unsigned char osabi);
+void print_type(unsigned int type, unsigned char data);
+void print_entry(unsigned long int entry, unsigned char class, unsigned char data);
+void close_elf(int elf);
+
 #endif
